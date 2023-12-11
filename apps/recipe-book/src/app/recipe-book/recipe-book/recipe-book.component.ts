a2e17d3bb8b0dc5005cb7c4abec2c720f6bc9224
Iurii <Iurii.Prytula@insert.com.pl>
2023-12-11 14:23:09 +0100

    using firebase

--- apps/recipe-book/src/app/recipe-book/recipe-book/recipe-book.component.ts
+++ apps/recipe-book/src/app/recipe-book/recipe-book/recipe-book.component.ts
@@ -5,14 +5,28 @@ import { recipes } from '../../const/recipes.const';
 import { RecipeListComponent } from "../recipe-list/recipe-list.component";
 import { RecipeDetailComponent } from "../recipe-detail/recipe-detail.component";
 import { RouterModule } from '@angular/router';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
-    selector: 'app-recipe-book',
-    standalone: true,
-    templateUrl: './recipe-book.component.html',
-    styleUrl: './recipe-book.component.scss',
-    imports: [CommonModule, RecipeListComponent, RecipeDetailComponent, RouterModule]
+  selector: 'app-recipe-book',
+  standalone: true,
+  templateUrl: './recipe-book.component.html',
+  styleUrl: './recipe-book.component.scss',
+  imports: [
+    CommonModule,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RouterModule,
+    HttpClientModule,
+  ],
 })
 export class RecipeBookComponent {
   bookRecipes: Recipe[] = recipes;
+
+  constructor(private http: HttpClient) {}
+
+  onFetchRecipes() {
+    this.http.get('https://recipe-book-4f550-default-rtdb.europe-west1.firebasedatabase.app/recipes.json')
+    .subscribe(response => console.log(response));
+  }
 }
